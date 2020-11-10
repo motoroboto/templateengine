7ecf1ce17c74011f4d8d86b21fef2e1aba89372f
motoroboto <robert.a.cullen@gmail.com>
2020-11-09 23:11:47 -0500

    wrote functions to render html output

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-// const { selectRole } = require("./lib/questions");ggh
 const mainTemplate = fs.readFileSync('./templates/main.html', 'utf8');
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const employeeList = [];
+const Constructors = {
+    Manager,
+    Engineer,
+    Intern,
+};
 
 
 const init = () => {
@@ -18,18 +23,34 @@ const init = () => {
 
 const buildEmployee = async (key) => {
     const result = await inquirer.prompt(questions[key]);
+    const employee = new Constructors[key](
+        result.employeeName,
+        result.employeeId,
+        result.employeeEmail,
+        result.specific,
+    );
+    employeeList.push(employee);
     moreEmployees();
 };
 
 const moreEmployees = async () => {
     const { more } = await inquirer.prompt(questions.moreEmployees);
-    if (more === "NO") return;
+    if (more === "NO") return buildHTML();
     selectRole();
 };
 
 const selectRole = async () => {
     const { role } = await inquirer.prompt(questions.selectRole);
-    if (role) buildEmployee(role);
+    buildEmployee(role);
+};
+
+const buildHTML = async () => {
+    const HTML = render(employeeList);
+    try {
+        fs.writeFileSync(outputPath, HTML);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 
