fbfd57a9193553174c00fb2b1c0c9ceef61f4738
motoroboto <robert.a.cullen@gmail.com>
2020-11-09 22:05:14 -0500

    reworked question functions

--- app.js
+++ app.js
@@ -2,56 +2,42 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const questions = require("./lib/questions");
+const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+// const { selectRole } = require("./lib/questions");ggh
 const mainTemplate = fs.readFileSync('./templates/main.html', 'utf8');
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
 
+const init = () => {
+    buildEmployee("Manager");
+};
+
+const buildEmployee = async (key) => {
+    const result = await inquirer.prompt(questions[key]);
+    moreEmployees();
+};
+
+const moreEmployees = async () => {
+    const { more } = await inquirer.prompt(questions.moreEmployees);
+    if (more === "NO") return;
+    selectRole();
+};
+
+const selectRole = async () => {
+    const { role } = await inquirer.prompt(questions.selectRole);
+    if (role) buildEmployee(role);
+};
+
+
+init();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function askQuestions() {
-    inquirer
-        .prompt(questions.managerQuestions)
-        .then((answer) => {
-            if (answer.addMore === 'YES') {
-                addEmployee()
-            } else {
-                writePage();
-            };
-        });
-};
-function addEmployee() {
-    inquirer
-        .prompt(questions.employeeQuestions)
-        .then((answer) => {
-            if (answer.addMore === 'YES') {
-                addEmployee()
-            } else {
-                writePage();
-            };
-        });
-}
-
-function writePage() {
-    fs.writeFile('output/index.html',
-
-        `${mainTemplate}`,
-
-        function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-}
-
-
-askQuestions();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
